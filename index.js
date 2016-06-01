2cad4cba60fb37884a1f84af4b38509148f8fb44
zhangtiantian10 <975571814@qq.com>
2016-06-01 13:38:12 +0800

    add get a item and insert item

--- index.js
+++ index.js
@@ -2,18 +2,87 @@ var express = require('express');
 var app = express();
 var fs = require("fs");
 
+var file = 'items.json';
+
 app.get('/listItems', function (req, res) {
-  fs.readFile("items.json", 'utf8', function (err, data) {
-    console.log( data );
-    res.end( data );
-  });
+    fs.readFile(file, 'utf8', function (err, data) {
+        if (err) {
+            res.status(404).end('未找到' + file + '文件!');
+
+            return;
+        }
+        data = JSON.parse(data);
+        res.status(200).json(data);
+    });
 });
 
-var server = app.listen(8081, function () {
+app.get('/listItems/:id', function (req, res) {
+    fs.readFile(file, "utf8", function (err, data) {
+        if (err) {
+            res.status(404).end('未找到' + file + '文件!');
 
-  var host = server.address().address
-  var port = server.address().port
+            return;
+        }
+        data = JSON.parse(data);
+        var item = data["item" + req.params.id];
+        if (item === undefined) {
+            res.status(404).end("未找到该商品!");
 
-  console.log("应用实例，访问地址为 http://%s:%s", host, port)
+            return;
+        }
+        res.status(200).json(item);
+    })
+});
+
+app.post('/listItems/:id', function (req, res) {
+    var item = {
+        "id": req.params.id,
+        "barcode": "ITEM000005",
+        "name": "方便面",
+        "unit": "袋",
+        "price": 3.50
+    };
+    fs.readFile(file, "utf8", function (err, data) {
+        if (err) {
+            res.status(404).end('未找到' + file + '文件!');
+
+            return;
+        }
+        data = JSON.parse(data);
+        var itemid = data['item' + req.params.id];
+        if (itemid != undefined) {
+            res.status(404).end("id为" + req.params.id + "的商品已存在！");
+
+            return;
+        }
+        data['item' + req.params.id] = item;
+        fs.writeFile(file, JSON.stringify(data), function (err) {
+            if(err){
+                res.status(404).end('未找到' + file + '文件!');
+
+                return;
+            }
+        })
+        res.status(200).json(item);
+    });
+});
+
+app.put('/listItems/:id',function (req, res) {
+
+    var item = {
+        "id": req.params.id,
+        "barcode": "ITEM000005",
+        "name": "方便面",
+        "unit": "袋",
+        "price": 3.50
+    }
+})
+
+var server = app.listen(3000, function () {
+
+    var host = server.address().address
+    var port = server.address().port
+
+    console.log("应用实例，访问地址为 http://%s:%s", host, port)
 
 })
