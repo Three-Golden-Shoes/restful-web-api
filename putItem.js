16e59cb2393482a170cc0281a4a0a6bd871fafe3
zhangtiantian10 <975571814@qq.com>
2016-06-01 20:33:14 +0800

    modify when json file not have item some problems

--- putItem.js
+++ putItem.js
@@ -17,7 +17,12 @@ router.put('/putItem/:id', function (req, res) {
     fs.readFile(fileName, "utf8", function (err, data) {
         if (err) {
             req.status(404).end(fileName + '文件不存在!');
-            
+
+            return;
+        }
+        if(data === ''){
+            res.status(404).end(fileName + '文件中没有商品数据!');
+
             return;
         }
         data = JSON.parse(data);
